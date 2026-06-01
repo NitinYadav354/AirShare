1df8d22c91bdb3abd6e26f1c8fb38febc1031e47
Nitin Yadav <nyadav4625@gmail.com>
2026-06-01 17:07:59 +0530

    update(UI): dark theme

--- AirShare/airshare/settings.py
+++ AirShare/airshare/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['https://airshare.nitinyadav.dev', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['airshare.nitinyadav.dev', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,6 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR ,'static'),]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
