7caa5609a60880e0a5d1fd6f8399eeffcd7e0f5e
Nitin Yadav <nyadav4625@gmail.com>
2026-06-04 00:53:40 +0530

    feat: delete files from cloudinary after fetching

--- AirShare/clipboard/views.py
+++ AirShare/clipboard/views.py
@@ -29,10 +29,6 @@ def fetch_clipboard(request):
             clipboard_item  = ClipboardItems.objects.get(UniqueCode = code)
             clipboard_item.text = process_text(clipboard_item.text)
             clipboard_item.delete()
-            # if clipboard_item.image:
-            #     os.remove(clipboard_item.image.path)
-            # if clipboard_item.documents:
-            #     os.remove(clipboard_item.documents.path)
             return render(request, 'clipboard/Clipboard.html', {'items': clipboard_item,'form': form})
         except ClipboardItems.DoesNotExist:
             return render(request, 'clipboard/Clipboard.html', {'error': "Not found", 'form': form})
