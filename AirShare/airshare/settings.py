16210236b52357293e25a92981eb28aabb544368
Nitin Yadav <nyadav4625@gmail.com>
2026-06-04 01:49:27 +0530

    fix: remove celery and redis

--- AirShare/airshare/settings.py
+++ AirShare/airshare/settings.py
@@ -159,14 +159,3 @@ if not DEBUG:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# celery configuration
-CELERY_BROKER_URL = env('CELERY_BROKER_URL', default="redis://localhost:7000/0")
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-
-CELERY_BEAT_SCHEDULE = {
-    'delete-expired-entries': {
-        'task': 'clipboard.tasks.delete_expired',
-        'schedule': 10.0,
-    },
-}
