3749f993d94a1696deac0312834437ea5e584286
Nitin Yadav <nyadav4625@gmail.com>
2026-08-23 22:46:58 +0530

    fix: correct DJANGO_SETTINGS_MODULE typo

--- AirShare/airshare/celery.py
+++ AirShare/airshare/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODLUE", "Airshare.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "airshare.settings")
 
 app = Celery('airshare')
 app.config_from_object('django.conf:settings', namespace='CELERY')
