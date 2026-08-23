e2c6d15440d857a0678007caa4d32f27bee5e9d0
Nitin Yadav <nyadav4625@gmail.com>
2026-08-23 22:19:51 +0530

    feat: add expires at field in schema as the single source of truth for timing

--- AirShare/clipboard/models.py
+++ AirShare/clipboard/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 import random
 
 # Create your models here.
@@ -8,15 +10,18 @@ class ClipboardItems(models.Model):
     image = models.ImageField(upload_to='uploads/images', blank=True, null=True )
     documents = models.FileField(upload_to='uploads/documents', blank=True, null=True )
     created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField(null=True, blank=True)
     isfetched = models.BooleanField(default=False)
-
     def save(self, *args, **kwargs):
+        is_new = self._state.adding
         if not self.UniqueCode:
             while True:
                 code = str(random.randint(1000,9999))
                 if not ClipboardItems.objects.filter(UniqueCode = code).exists():
                     self.UniqueCode = code
                     break
+        if is_new and not self.expires_at:
+            self.expires_at = timezone.now() + timedelta(minutes=15)
         super().save(*args, **kwargs)
 
     def __str__(self):
