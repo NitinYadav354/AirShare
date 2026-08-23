470a075cc7c975a91a38ca58b56a51500f8ad61c
Nitin Yadav <nyadav4625@gmail.com>
2026-08-23 22:15:22 +0530

    fix: typo causing configuration error

--- AirShare/airshare/celery.py
+++ AirShare/airshare/celery.py
@@ -4,5 +4,5 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODLUE", "Airshare.settings")
 
 app = Celery('airshare')
-app.config_from_object('django_conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
